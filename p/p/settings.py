92a7e128d672077ace6a3ce239a38a800e1d4fd9
Civil-py <civilmotha@gmail.com>
2025-04-10 16:29:18 +0200

    Update settings.py

--- p/p/settings.py
+++ p/p/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'cusp',
+    'p/cusp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
