0bd85c34ffff89650b2a13eec06fcdebcec2bc64
Civil-py <civilmotha@gmail.com>
2025-05-29 10:51:26 +0200

    Update urls.py

--- cusp/urls.py
+++ cusp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("get-quotation", views.quote, name="quote"),
+    path('sitemap.xml', TemplateView.as_view(template_name='cusp/sitemap.xml', content_type='application/xml')),
 
 ]
 
